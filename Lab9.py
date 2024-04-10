ebea463e00b97b624058091f548b5a393d51e032
kbalsimo <166614559+kbalsimo@users.noreply.github.com>
2024-04-10 08:37:38 -0400

    Update Lab9.py

--- Lab9.py
+++ Lab9.py
@@ -24,5 +24,16 @@ def encoder(password):
     return output
 
 
+def decoder(password):
+    decoding = []
+    output = ""
+    for num in password:
+        decoding.append(int(num))
+    for num in decoding:
+        num -= 3
+        output += str(num)
+    return output
+
+
 if __name__ == "__main__":
     main()
